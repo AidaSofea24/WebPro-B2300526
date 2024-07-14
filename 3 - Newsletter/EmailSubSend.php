aeac16097002a79cdb38290f5b3b76cb23e1d2a2
Burney04 <119722932+Burney04@users.noreply.github.com>
2024-07-14 14:14:55 +0800

    newsletter feature

---
+++ 3 - Newsletter/EmailSubSend.php
@@ -0,0 +1,37 @@
+<?php
+use PHPmailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+    $mail = new PHPMailer(true);
+    
+    $mail -> isSMTP();
+    $mail -> Host = 'smtp.gmail.com'; 
+    $mail -> SMTPAuth = true;
+    $mail -> Username = 'helpuniversitystudentcenter@gmail.com';
+    $mail -> Password = 'qdcy typj qcbl kzgq';
+    $mail -> SMTPSecure = 'ssl';
+    $mail -> Port = 465;
+
+    $mail->setFrom('helpuniversitystudentcenter@gmail.com', 'Help University Student Center');
+
+    $mail->addAddress($_POST["email"]);
+
+    $mail->isHTML(true);
+
+    $mail->Subject = $_POST["subject"];
+    $mail->Body = $_POST["message"];
+
+    $mail->send();
+
+    echo "<script>
+    alert('Sent Successfully');
+    document.location.href = 'emailSub.php';
+    </script>
+    ";
+}
+?> 

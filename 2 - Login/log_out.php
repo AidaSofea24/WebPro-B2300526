5a4434933630f3e5a8921196cd2a84c3e76ceebf
Burney04 <119722932+Burney04@users.noreply.github.com>
2024-07-15 22:36:41 +0800

    added some comments

--- 2 - Login/log_out.php
+++ 2 - Login/log_out.php
@@ -3,9 +3,11 @@ session_start();
 
 // Check if the logout action is confirmed
 if (isset($_GET['log_out']) && $_GET['log_out'] == 'true') {
+    
+    // if the logout action is confirmed session is destroyed and user will log out
     session_unset();
     session_destroy();
-    header("Location: index.html");
+    header("Location: index.html"); //user will be redirected back to the login page
     exit();
 }
 ?>
